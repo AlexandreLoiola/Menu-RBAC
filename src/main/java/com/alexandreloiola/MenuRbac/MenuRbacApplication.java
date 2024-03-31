e0129fe3dd194688bf4d0ec3d6016065c527c827
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-03-31 18:53:51 -0300

    chore: Initial Config

---
+++ src/main/java/com/alexandreloiola/MenuRbac/MenuRbacApplication.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.MenuRbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuRbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandreloiola/MenuRbac/MenuRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.MenuRbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
